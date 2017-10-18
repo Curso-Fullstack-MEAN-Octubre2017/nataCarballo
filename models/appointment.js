16361c4324a6b987897fa0aa4e03e193d6f37689
Natalia <Natalia.home>
2017-10-18 22:15:05 +0200

    busqueda appointment por id

--- models/appointment.js
+++ models/appointment.js
@@ -3,13 +3,13 @@ const Schema = mongoose.Schema;
 
 
 const AppointmentSchema = new Schema({
-	dateStart:String,
-	dateEnd:String,
-	petId:String, 
+	dateStart:String,//{type:Date, require:true},
+	dateEnd:String,//{type:Date, require:true},
+	petId:String, //{type: Schema.ObjectId, ref:'pet',required:true},
 	vetId:String,
-	status:String
+	status:String//{type: Number, enum [-1,0,1,2]}
 		
-		//{type: Number, enum [-1,0,1,2]}
+		
 });
 
 AppointmentSchema.pre('save',(next)=>{next();});
--- routes/appointments.js
+++ routes/appointments.js
@@ -1,33 +1,45 @@
 const Appointment= require('../models/appointment');
 var moment = require('moment');
+const Customer= require('../models/customer');
+const Pet= require('../models/pets');
 
 module.exports = (router) => {
 	
 	
 	router.get('/appointments', (req, res, next)=> {
 		Appointment.find({}, (err, appointment)=> {
-			if (err){
-				console.error(err);
-				return next();
-			}
+			if (err) return next();
 			res.send(200,appointment);
 	    });
 	});
 	router.get('/appointments/:id',(req, res,next)=> {
-		Appointment.findById({_id:req.params.id}, (err, Appointment)=> {
+		Appointment.findById({_id:req.params.id}, (err, appointments)=> {
 			if (err) return next();
-			res.json(Appointment);
+			res.json(appointments);
 			
 		});
 	});
 	
-	router.get('/appointments/:from/:to',(req, res)=>{
-		var from=req.params.from;
-		var to=req.params.to;
+	router.get('/appointments/:fromdate/:todate',(req, res)=>{
+		var from = moment(req.params.fromdate,'YYYYMM');
+		var to = moment(req.params.todate,'YYYYMM');
 		
-		from= moment();
-		to= moment();
-		//falta por completar, investigar moment.
+		Appointment.find({dateStart : {"$gte": from, "$lt": to}},(err, appointment)=>{
+			if (err) return next();
+			res.json(appointment);
+		});/*.populate(
+			{
+	            path: 'pet',
+	            model: 'pets',
+	            select: 'name',
+	            populate: {
+	                path: 'customer',
+	                model: 'customer',
+	                select: 'firstName lastName'
+	            }
+			}
+		
+		).sort({'dateTime': 1})*/
 	});
 	
 	router.put('/appointments/:id', (req, res, next) => {
